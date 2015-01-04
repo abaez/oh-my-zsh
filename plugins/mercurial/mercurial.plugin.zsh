82750c5ab7a166bfb783e975745e1a202d07d327
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-04 17:13:26 -0500

    removed use for branch through id fn

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -30,14 +30,14 @@ function hg_get_branch_name() {
 }
 
 function hg_get_id() {
-    echo "$(hg id)"
+    echo "$(hg id -i -b -B -t)"
 }
 
 
 
 function hg_prompt_info {
   if [ $(in_hg) ]; then
-    _DISPLAY="$(hg_get_branch_name) $(hg_get_id)"
+    _DISPLAY="$(hg_get_id)"
     echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_PREFIX\
 $ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR$(hg_dirty)$ZSH_PROMPT_BASE_COLOR"
     unset _DISPLAY
